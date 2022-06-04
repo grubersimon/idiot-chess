b3d643aa2ddfbcfc688e639f4c795ca027d7a0d4
grubersimon <20182008@atn.ac.at>
2022-06-04 20:11:44 +0200

    #00 - intro
    - welcome window
    - unstable: game window

---
+++ app/src/main/java/com/example/idiot_chess/GameFragment.kt
@@ -0,0 +1,32 @@
+package com.example.idiot_chess
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_game, container, false)
+        val imgBtn = view.findViewById<Button>(R.id.imageButton2)
+
+        imgBtn.setOnClickListener {
+            imgBtn.setBackgroundResource(R.drawable.x_field)
+        }
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/idiot_chess/WelcomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.idiot_chess
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startBtn = view.findViewById<Button>(R.id.btnStartGame)
+
+        startBtn.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeToGameFragment)
+        }
+            return view
+    }
+}
