c58765b16df06e75521bd31080a79cf86d4f5562
grubersimon <20182008@atn.ac.at>
2022-06-10 13:24:35 +0200

    #02 result
    - result fragment works

--- app/src/main/java/com/example/idiot_chess/GameFragment.kt
+++ app/src/main/java/com/example/idiot_chess/GameFragment.kt
@@ -1,6 +1,5 @@
 package com.example.idiot_chess
 
-import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,17 +8,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import com.example.idiot_chess.databinding.ActivityMainBinding.inflate
 
-class GameFragment : Fragment() {
+class GameFragment : Fragment(R.layout.fragment_game) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView
+    (
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View?
+    {
 
         val view = inflater.inflate(R.layout.fragment_game, container, false)
         var currentPlayer = 0
         var sumMoves = 0
+
 
         val field0_0 = view.findViewById<Button>(R.id.field0_0) as ImageView
         val field0_1 = view.findViewById<Button>(R.id.field0_1) as ImageView
@@ -32,25 +36,46 @@ class GameFragment : Fragment() {
         val field2_2 = view.findViewById<Button>(R.id.field2_2) as ImageView
         val curPlayer = view.findViewById<Button>(R.id.imgCurrentPlayer) as ImageView
 
-
-        fun whoIsWinner(x: String)
+        fun shareRes(x: ImageView)
         {
-            
 
+            if(x.getTag() == "x")
+            {
+                val action = GameFragmentDirections
+                    .actionGameToResultFragment(1)
+                view.findNavController()
+                    .navigate(action)
+            }
+            else if (x.getTag() == "o")
+            {
+                val action = GameFragmentDirections
+                    .actionGameToResultFragment(2)
+                view.findNavController()
+                    .navigate(action)
+            }
         }
 
+        fun whoIsWinner (player: String)
+        {
+            if (field0_0.getTag() == player && field0_1.getTag() == player && field0_2.getTag() == player)         {shareRes(field0_0)}
+            else if (field1_0.getTag() == player && field1_1.getTag() == player && field1_2.getTag() == player)    {shareRes(field0_1)}
+            else if (field2_0.getTag() == player && field2_1.getTag() == player && field2_2.getTag() == player)    {shareRes(field2_0)}
+            else if (field0_0.getTag() == player && field1_0.getTag() == player && field2_0.getTag() == player)    {shareRes(field0_0)}
+            else if (field0_1.getTag() == player && field1_1.getTag() == player && field2_1.getTag() == player)    {shareRes(field1_1)}
+            else if (field0_2.getTag() == player && field1_2.getTag() == player && field2_2.getTag() == player)    {shareRes(field0_2)}
+            else if (field0_0.getTag() == player && field1_1.getTag() == player && field2_2.getTag() == player)    {shareRes(field0_0)}
+            else if (field0_2.getTag() == player && field1_1.getTag() == player && field2_0.getTag() == player)    {shareRes(field0_2)}
+        }
 
         fun game(x: ImageView)
         {
-
             if(currentPlayer == 0  && x.getTag() == "empty")
             {
                 x.setImageResource(R.drawable.x_field)
                 curPlayer.setImageResource(R.drawable.o_field)
                 currentPlayer = 1
                 sumMoves = sumMoves + 1
-                x.setTag("set")
-
+                x.setTag("x")
             }
             else if(x.getTag() == "empty")
             {
@@ -58,14 +83,24 @@ class GameFragment : Fragment() {
                 curPlayer.setImageResource(R.drawable.x_field)
                 currentPlayer = 0
                 sumMoves = sumMoves + 1
-                x.setTag("set")
+                x.setTag("o")
+            }
+
+            if (sumMoves > 4)
+            {
+                whoIsWinner("x")
+                whoIsWinner("o")
             }
 
             if(sumMoves == 9)
             {
-                view.findNavController().navigate(R.id.action_gameToResultFragment)
+                val action = GameFragmentDirections
+                    .actionGameToResultFragment(0)
+                view.findNavController()
+                    .navigate(action)
             }
         }
+
 
         field0_0.setOnClickListener {
             game(field0_0)
@@ -105,4 +140,5 @@ class GameFragment : Fragment() {
 
         return view
     }
+
 }
--- app/src/main/java/com/example/idiot_chess/ResultFragment.kt
+++ app/src/main/java/com/example/idiot_chess/ResultFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 
 
@@ -24,10 +25,25 @@ class ResultFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_result, container, false)
         val startBtn = view.findViewById<Button>(R.id.btnRestartGame)
 
+        val winnerInput = ResultFragmentArgs.fromBundle(requireArguments()).winnerInput
+
+        val textView: TextView = view.findViewById(R.id.winnerText) as TextView
+            if (winnerInput == 0)
+            {
+                textView.text = getString(R.string.undecided)
+            }
+            else if (winnerInput == 1)
+            {
+                textView.text = getString(R.string.x_won)
+            }
+            else
+            {
+                textView.text = getString(R.string.o_won)
+            }
+
         startBtn.setOnClickListener {
             view.findNavController().navigate(R.id.action_resultToGameFragment)
         }
         return view
-
     }
 }
